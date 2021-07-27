732edda579edc31ee3becf2225b76096aeff7770
Yusif Aghalar <yusif.aghalarli@novum.tech>
2021-07-27 15:58:33 +0400

    Add Catalog Controller

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("MongoSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("MongoSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("MongoSettings:CollectionName"));
+
+        }
+        public IMongoCollection<Product> Products { get; init; }
+    }
+}
